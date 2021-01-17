21ea7c4246dbb0c4216b1c44a01315d1386f6f53
chivinh <vinh.lam@itbeesolutions.vn>
2021-01-18 00:52:12 +0700

    neww

---
+++ src/net/javaguides/usermanagement/web/UserServlet.java
@@ -0,0 +1,64 @@
+package net.javaguides.usermanagement.web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.javaguides.usermanagement.dao.UserDAO;
+
+/**
+ * Servlet implementation class UserServlet
+ */
+@WebServlet("/")
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private UserDAO userDAO;
+    /**
+     * Default constructor. 
+     */
+    public UserServlet() {
+        this.userDAO = new UserDAO();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doGet(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String action = request.getServletPath();
+		
+		switch (action) {
+		case "/new":
+			showNewForm(request, response);
+			break;
+		case "/insert":
+			break;
+		case "/delete":
+			break;
+		case "/edit":
+			break;
+		case "/update":
+			break;
+		default:
+			break;
+		
+		}
+	}
+	
+	private void showNewForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("user-form.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
